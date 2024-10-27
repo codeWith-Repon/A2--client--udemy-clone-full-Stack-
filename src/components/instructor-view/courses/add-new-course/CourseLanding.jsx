99ec1fca376bfa9aae8f64b0f84da1a6fed161a8
Md Repon <repon7253@gmail.com>
2024-10-27 20:13:48 +0600

    course landing form design complite

--- src/components/instructor-view/courses/add-new-course/CourseLanding.jsx
+++ src/components/instructor-view/courses/add-new-course/CourseLanding.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import FormControls from "@/components/common-form/form-controls";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { courseLandingPageFormControls } from "@/config";
+import { InstructorContext } from "@/context/instructor-context";
+import React, { useContext } from "react";
 
 const CourseLanding = () => {
+  const { courseLandingFormData, setCourseLandingFormData } =
+    useContext(InstructorContext);
   return (
-    <div>
-      CourseLanding
-      CourseLanding
-    </div>
-  )
-}
+    <Card>
+      <CardHeader>
+        <CardTitle>Course Landing Page</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <FormControls
+          formControls={courseLandingPageFormControls}
+          formData={courseLandingFormData}
+          setFormData={setCourseLandingFormData}
+        />
+      </CardContent>
+    </Card>
+  );
+};
 
-export default CourseLanding
+export default CourseLanding;
