bd86b755fed0e35a5b3d040cbe03b8f525a464b5
Md Repon <repon7253@gmail.com>
2024-10-28 11:17:31 +0600

    complite basic ui and when click add button than now course add (index.js> index.jsx> courseCurriculum.jsx)

--- src/components/instructor-view/courses/add-new-course/CourseCurriculum.jsx
+++ src/components/instructor-view/courses/add-new-course/CourseCurriculum.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
+import { InstructorContext } from "@/context/instructor-context";
+import React, { useContext } from "react";
 
 const CourseCurriculum = () => {
-  return (
-    <div>
-      CourseCurriculum
-    </div>
-  )
-}
+  const { courseCurriculumFormData, setCourseCurriculumFormData } =
+    useContext(InstructorContext);
 
-export default CourseCurriculum
+    function handleNewLecture(){
+      setCourseCurriculumFormData([
+        ...courseCurriculumFormData,
+        {
+          ...courseCurriculumFormData[0]
+        }
+      ])
+    }
+
+    console.log(courseCurriculumFormData)
+  return (
+    <Card>
+      <CardHeader>Create Course Curriculum</CardHeader>
+      <CardContent>
+        <Button onClick={handleNewLecture}>Add Lecture</Button>
+        <div className="mt-4 space-y-4">
+          {courseCurriculumFormData.map((curriculumItem, index) => (
+            <div key={`title-${index + 1}`} className="border p-5 rounded-md">
+              <div className="flex gap-5 items-center">
+                <h3 className="font-semibold">Lecture {index + 1}</h3>
+                <Input
+                  name={`title-${index + 1}`}
+                  placeholder="Enter lecture title"
+                  className="max-w-96"
+                />
+                <div className="flex items-center space-x-2">
+                  <Switch checked={true} id={`freePreview-${index + 1}`} />
+                  <Label htmlFor={`freePreview-${index + 1}`}>
+                    Free Preview
+                  </Label>
+                </div>
+              </div>
+              <div className="mt-6">
+                <Input type="file" accept="video/*" className="mb-4" />
+              </div>
+            </div>
+          ))}
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default CourseCurriculum;
--- src/context/instructor-context/index.jsx
+++ src/context/instructor-context/index.jsx
@@ -1,4 +1,7 @@
-import { courseLandingInitialFormData } from "@/config";
+import {
+  courseCurriculumInitialFormData,
+  courseLandingInitialFormData,
+} from "@/config";
 import { createContext, useState } from "react";
 
 export const InstructorContext = createContext(null);
@@ -7,9 +10,17 @@ export default function InstructorProvider({ children }) {
   const [courseLandingFormData, setCourseLandingFormData] = useState(
     courseLandingInitialFormData
   );
+  const [courseCurriculumFormData, setCourseCurriculumFormData] = useState(
+    courseCurriculumInitialFormData
+  );
   return (
     <InstructorContext.Provider
-      value={{ courseLandingFormData, setCourseLandingFormData }}
+      value={{
+        courseLandingFormData,
+        setCourseLandingFormData,
+        courseCurriculumFormData,
+        setCourseCurriculumFormData,
+      }}
     >
       {children}
     </InstructorContext.Provider>
