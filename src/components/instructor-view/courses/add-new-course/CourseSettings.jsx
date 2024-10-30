711fd39600857d816889e74c1257ebb7abae8f4b
Md Repon <repon7253@gmail.com>
2024-10-30 10:15:44 +0600

    successfully update course image

--- src/components/instructor-view/courses/add-new-course/CourseSettings.jsx
+++ src/components/instructor-view/courses/add-new-course/CourseSettings.jsx
@@ -1,19 +1,55 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { InstructorContext } from "@/context/instructor-context";
+import { mediaUploadService } from "@/services";
 import { Label } from "@radix-ui/react-label";
-import React from "react";
+import React, { useContext } from "react";
 
 const CourseSettings = () => {
+  const { courseLandingFormData, setCourseLandingFormData } =
+    useContext(InstructorContext);
+
+  async function handleImageUploadChange(event) {
+    const selectedImage = event.target.files[0];
+
+    if (selectedImage) {
+      const imageFormData = new FormData();
+      imageFormData.append("file", selectedImage);
+
+      try {
+        const response = await mediaUploadService(imageFormData);
+
+        console.log("response image", response);
+        if (response.success) {
+          setCourseLandingFormData({
+            ...courseLandingFormData,
+            image: response.data.url,
+          });
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+
   return (
     <Card>
       <CardHeader>
         <CardTitle>Course Settings</CardTitle>
       </CardHeader>
       <CardContent>
-        <div className="flex flex-col gap-3">
-          <Label>Upload Course Image</Label>
-          <Input type="file" accept="image/*" />
-        </div>
+        {courseLandingFormData?.image ? (
+          <img src={courseLandingFormData.image} />
+        ) : (
+          <div className="flex flex-col gap-3">
+            <Label>Upload Course Image</Label>
+            <Input
+              onChange={handleImageUploadChange}
+              type="file"
+              accept="image/*"
+            />
+          </div>
+        )}
       </CardContent>
     </Card>
   );
