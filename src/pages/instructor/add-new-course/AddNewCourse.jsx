d7be2aedda573b89bbb95d032affca00e0588698
Md Repon <repon7253@gmail.com>
2024-11-06 14:09:57 +0600

    create course function and log course data

--- src/pages/instructor/add-new-course/AddNewCourse.jsx
+++ src/pages/instructor/add-new-course/AddNewCourse.jsx
@@ -4,12 +4,23 @@ import CourseSettings from "@/components/instructor-view/courses/add-new-course/
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  courseCurriculumInitialFormData,
+  courseLandingInitialFormData,
+} from "@/config";
+import { AuthContext } from "@/context/auth-context";
 import { InstructorContext } from "@/context/instructor-context";
+import { addNewCourseService } from "@/services";
 import React, { useContext } from "react";
 
 const AddNewCoursePage = () => {
-  const { courseCurriculumFormData, courseLandingFormData } =
-    useContext(InstructorContext);
+  const {
+    courseCurriculumFormData,
+    courseLandingFormData,
+    setCourseLandingFormData,
+    setCourseCurriculumFormData,
+  } = useContext(InstructorContext);
+  const { auth } = useContext(AuthContext);
 
   function isEmpty(value) {
     if (Array.isArray(value)) {
@@ -44,6 +55,21 @@ const AddNewCoursePage = () => {
     return hasFreePreview;
   }
 
+  async function handleCreateCourse() {
+    const courseFinalFormData = {
+      instructorId: auth?.user?._id,
+      instructorName: auth?.user?.userName,
+      date: new Date(),
+      ...courseLandingFormData,
+      students: [],
+      curriculum: courseCurriculumFormData,
+      isPublished: true,
+    };
+
+
+    console.log(courseFinalFormData, "courseFinalFormData");
+  }
+
   return (
     <div className="container mx-auto p-4">
       <div className="flex justify-between">
@@ -51,6 +77,7 @@ const AddNewCoursePage = () => {
         <Button
           disabled={!validateFormData()}
           className="text-sm tracking-wider font-bold px-8"
+          onClick={handleCreateCourse}
         >
           Submit
         </Button>
