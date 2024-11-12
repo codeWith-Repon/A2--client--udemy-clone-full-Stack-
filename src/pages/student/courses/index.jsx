0217fe8b8b4c6feac2f981796618dffa8bb1eae5
Md Repon <repon7253@gmail.com>
2024-11-12 11:11:06 +0600

    customized sort ui

--- src/pages/student/courses/index.jsx
+++ src/pages/student/courses/index.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { sortOptions } from "@/config";
+import { ArrowUpDownIcon } from "lucide-react";
+import React, { useState } from "react";
 
 const StudentViewCoursesPage = () => {
+  const [sort, setSort] = useState("");
   return (
-    <div>
-      StudentViewCoursesPage
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">All Courses</h1>
+      <div className="flex flex-col md:flex-row gap-4">
+        <aside className="w-full md:w-64 space-y-4">
+          <div>//filter option here</div>
+        </aside>
+        <main className="flex-1">
+          <div className="flex justify-end items-center mb-4 gap-5">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="sm"
+                  className="flex items-center gap-2 p-5"
+                >
+                  <ArrowUpDownIcon className="h-4 w-4" />
+                  <span className="text-[16px] font-medium">Sort By</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end" className="w-[180px]">
+                <DropdownMenuRadioGroup
+                  value={sort}
+                  onValueChange={(value) => setSort(value)}
+                >
+                  {sortOptions.map((sortItem) => (
+                    <DropdownMenuRadioItem
+                      value={sortItem.id}
+                      key={sortItem.id}
+                    >
+                      {sortItem.label}
+                    </DropdownMenuRadioItem>
+                  ))}
+                </DropdownMenuRadioGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </main>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default StudentViewCoursesPage
+export default StudentViewCoursesPage;
