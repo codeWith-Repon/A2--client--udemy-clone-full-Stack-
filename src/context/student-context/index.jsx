65d867a4957cddb1a041b2ea019a98025a45025f
Md Repon <repon7253@gmail.com>
2024-11-14 11:06:19 +0600

    single course details log the console

--- src/context/student-context/index.jsx
+++ src/context/student-context/index.jsx
@@ -5,6 +5,9 @@ export const StudentContext = createContext(null);
 export default function StudentProvider({ children }) {
   const [studentViewCoursesList, setStudentViewCoursesList] = useState([]);
   const [loadingState, setLoadingState] = useState(true);
+  const [StudentViewCourseDetailsPage, setStudentViewCourseDetailsPage] =
+    useState(null);
+  const [currentCourseDetailsId, setCurrentCourseDetailsId] = useState(null);
 
   return (
     <StudentContext.Provider
@@ -13,6 +16,10 @@ export default function StudentProvider({ children }) {
         setStudentViewCoursesList,
         loadingState,
         setLoadingState,
+        StudentViewCourseDetailsPage,
+        setStudentViewCourseDetailsPage,
+        currentCourseDetailsId,
+        setCurrentCourseDetailsId,
       }}
     >
       {children}
--- src/pages/student/course-details/index.jsx
+++ src/pages/student/course-details/index.jsx
@@ -1,6 +1,38 @@
-import React from "react";
+import { StudentContext } from "@/context/student-context";
+import { fetchStudentViewCourseDetailsService } from "@/services";
+import React, { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const StudentViewCourseDetailsPage = () => {
+  //1st
+  const {
+    StudentViewCourseDetailsPage,
+    setStudentViewCourseDetailsPage,
+    currentCourseDetailsId,
+    setCurrentCourseDetailsId,
+  } = useContext(StudentContext);
+
+  const { id } = useParams();
+
+  //4th
+  async function fetchStudentViewCourseDetails() {
+    const response = await fetchStudentViewCourseDetailsService(
+      currentCourseDetailsId
+    );
+
+    console.log(response, "response");
+  }
+
+  //3rd
+  useEffect(() => {
+    if (currentCourseDetailsId !== null) fetchStudentViewCourseDetails();
+  }, [currentCourseDetailsId]);
+
+  //2nd
+  useEffect(() => {
+    if (id) setCurrentCourseDetailsId(id);
+  }, [id]);
+
   return <div>StudentViewCourseDetailsPage</div>;
 };
 
